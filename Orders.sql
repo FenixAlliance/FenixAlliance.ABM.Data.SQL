867d2cd260704744975a2696544861896e6b4c3b
Daniel Lozano <d.lozanonavas@outlook.com>
2021-12-29 17:04:21 -0500

    Improve Queries

--- Orders.sql
+++ Orders.sql
@@ -1,1 +1,17 @@
 -- Get orders Data
+SELECT u.Email, b.BusinessLegalName, o.* 
+FROM absuite_prod.Order o
+JOIN aspnetusers u
+	ON o.AccountHolderID = u.Id
+JOIN Business b
+	ON b.ID = o.BusinessID;
+
+-- Fix Empty Order Titles
+UPDATE absuite_prod.Order
+SET Title = ""
+WHERE Title IS NULL;
+
+-- Fix Empty Order Descriptions
+UPDATE absuite_prod.Order
+SET Description = ""
+WHERE Description IS NULL;
