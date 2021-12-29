54f66435e609086a11fa5479742e01d5408a2939
Daniel Lozano <d.lozanonavas@outlook.com>
2021-12-29 16:47:26 -0500

    Adds Global Data Queries

---
+++ GlobalData.sql
@@ -0,0 +1,17 @@
+-- Get all countries
+SELECT * FROM country;
+
+-- Get all currencies
+SELECT * FROM currency;
+
+-- Get all timezones
+SELECT * FROM timezone;
+
+-- Get all timezones
+SELECT * FROM countrylanguage;
+
+-- Get all Country States (COUNT)
+SELECT COUNT(*) FROM countrystate;
+
+-- Get all Country Cities (COUNT)
+SELECT COUNT(*) FROM city;
