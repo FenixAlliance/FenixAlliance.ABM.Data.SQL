10ff9917c4754f366220882cdbd688c5fc7c50f0
Daniel Lozano <d.lozanonavas@outlook.com>
2022-05-05 01:09:55 -0500

    Minors

---
+++ OAuthApplications.sql
@@ -0,0 +1,12 @@
+SELECT * FROM openiddictentityframeworkcorescope;
+SELECT * FROM openiddictentityframeworkcoretoken;
+SELECT * FROM openiddictentityframeworkcoreauthorization;
+SELECT * FROM openiddictentityframeworkcoreapplication;
+
+TRUNCATE openiddictentityframeworkcoretoken;
+TRUNCATE openiddictentityframeworkcorescope;
+TRUNCATE openiddictentityframeworkcoreauthorization;
+TRUNCATE openiddictentityframeworkcoreapplication;
+
+
+
